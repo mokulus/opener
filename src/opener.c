9e944203e69d60a8f0e2256792652e5b01d10569
Mateusz Okulus <mmokulus@gmail.com>
2021-08-03 12:05:17 +0200

    Add -f option. Simplify common prefix removing.

--- src/opener.c
+++ src/opener.c
@@ -22,7 +22,7 @@ static int get_term_lines()
 
 static void usage()
 {
-	fprintf(stderr, "opener [-r] [-d] program regex path\n");
+	fprintf(stderr, "opener [-r] [-d] [-f] program regex path\n");
 }
 
 static char *alloc_sprintf(const char *fmt, ...)
@@ -106,18 +106,17 @@ static pid_t selector_pipe(int npipe[2], int lines, const char *prompt)
 		close(pre_pipe[1]);
 		close(post_pipe[0]);
 
-		const char *fmt = "fzy -p '%s' -l %d 0>&%d 1>&%d";
-		char *str = alloc_sprintf(fmt,
-					  prompt,
-					  lines,
-					  pre_pipe[0],
-					  post_pipe[1]);
-
 		/* (void)lines; */
-		/* const char *fmt = "fzf --reverse --prompt='%s' 0>&%d 1>&%d";
-		 */
-		/* char *str = */
-		/*     alloc_sprintf(fmt, prompt, pre_pipe[0], post_pipe[1]); */
+		/* const char *fmt = "fzy -p '%s' 0>&%d 1>&%d"; */
+		/* char *str = alloc_sprintf(fmt, */
+		/* 			  prompt, */
+		/* 			  pre_pipe[0], */
+		/* 			  post_pipe[1]); */
+
+		(void)lines;
+		const char *fmt = "fzf --reverse --prompt='%s' 0>&%d 1>&%d";
+		char *str =
+		    alloc_sprintf(fmt, prompt, pre_pipe[0], post_pipe[1]);
 
 		execlp("st", "st", "-e", "sh", "-c", str, (char *)NULL);
 	} else if (exec_pid == -1) {
@@ -137,8 +136,8 @@ static int fts_strcmp_path(const FTSENT **a, const FTSENT **b)
 	return strcmp((*a)->fts_path, (*b)->fts_path);
 }
 
-static void
-write_files(int fd, char *dirpath, const char *regex_str, int allow_dirs)
+static void write_files(
+    int fd, char *dirpath, const char *regex_str, int use_dirs, int use_files)
 {
 	regex_t reg;
 	if (regcomp(&reg, regex_str, REG_EXTENDED | REG_ICASE | REG_NOSUB)) {
@@ -161,28 +160,23 @@ write_files(int fd, char *dirpath, const char *regex_str, int allow_dirs)
 				goto fail_fts_read;
 			}
 		}
+		if (ent->fts_level == 0) {
+			continue;
+		}
 		if (ent->fts_info == FTS_DP) {
 			continue;
 		}
-		if (!allow_dirs && ent->fts_info == FTS_D) {
+		if (ent->fts_info == FTS_D && !use_dirs) {
 			continue;
 		}
 		if (ent->fts_info == FTS_F &&
-		    regexec(&reg, ent->fts_name, 0, NULL, 0)) {
+		    (!use_files || regexec(&reg, ent->fts_name, 0, NULL, 0))) {
 			continue;
 		}
-		size_t slash_count = 0;
-		for (size_t i = 0;; ++i) {
-			if (!dirpath[i])
-				break;
-			if (dirpath[i] == '/')
-				slash_count++;
-		}
-		char *cut = ent->fts_path;
-		/* + 1 to also skip the middle slash */
-		for (size_t i = 0; i < slash_count + 1; ++i)
-			cut = strchr(cut, '/') + 1;
-		write(fd, cut, strlen(cut));
+		size_t common_len = strlen(dirpath) + 1;
+		write(fd,
+		      ent->fts_path + common_len,
+		      ent->fts_pathlen - common_len);
 		write(fd, "\n", 1);
 	}
 fail_fts_read:
@@ -194,7 +188,8 @@ fail_fts:
 static char *pick_path(char *dirpath,
 		       const char *regex_str,
 		       int lines,
-		       int allow_dirs,
+		       int use_dirs,
+		       int use_files,
 		       const char *program)
 {
 	char *line = NULL;
@@ -203,7 +198,7 @@ static char *pick_path(char *dirpath,
 	pid_t id;
 	if ((id = selector_pipe(pipe, lines, prompt)) == -1)
 		goto fail;
-	write_files(pipe[1], dirpath, regex_str, allow_dirs);
+	write_files(pipe[1], dirpath, regex_str, use_dirs, use_files);
 	close(pipe[1]);
 	char *name = read_line_fd(pipe[0]);
 	close(pipe[0]);
@@ -214,9 +209,9 @@ static char *pick_path(char *dirpath,
 	size_t name_len = strlen(name);
 	line = malloc(dirpath_len + name_len + 2);
 	memcpy(line, dirpath, dirpath_len);
-	memcpy(line + dirpath_len, "/", 1);
+	line[dirpath_len] = '/';
 	memcpy(line + dirpath_len + 1, name, name_len);
-	memcpy(line + dirpath_len + 1 + name_len, "\0", 1);
+	line[dirpath_len + 1 + name_len] = '\0';
 	free(name);
 fail:
 	free(prompt);
@@ -252,39 +247,52 @@ int main(int argc, char *argv[])
 
 	int opt;
 	int remove = 0;
-	int allow_dirs = 0;
-	while ((opt = getopt(argc, argv, "rd")) != -1) {
+	int use_dirs = 0;
+	int use_files = 0;
+	while ((opt = getopt(argc, argv, "rdf")) != -1) {
 		switch (opt) {
 		case 'r':
 			remove = 1;
 			break;
 		case 'd':
-			allow_dirs = 1;
+			use_dirs = 1;
+			break;
+		case 'f':
+			use_files = 1;
 			break;
 		default:
 			usage();
-			return -1;
+			return 1;
 		}
+	}
+
+	if (!(use_dirs || use_files)) {
+		fputs("Need at least one of -f, -d\n", stderr);
+		usage();
+		return 1;
 	}
 
 	if (optind + 2 >= argc) {
 		usage();
-		return -1;
+		return 1;
 	}
 
 	char *program = argv[optind++];
 	char *regex_str = argv[optind++];
-	char *dirpath = argv[optind++];
+	/* ensure no trailing slash, we assume this later */
+	char *dirpath = realpath(argv[optind++], NULL);
+	if (!dirpath)
+		return 1;
 	char *fullpath =
-	    pick_path(dirpath, regex_str, lines, allow_dirs, program);
+	    pick_path(dirpath, regex_str, lines, use_dirs, use_files, program);
 	if (!fullpath)
-		return -1; /* TODO */
+		return 1;
 
 	pid_t program_pid = fork();
 	if (program_pid == 0) {
 		execlp(program, program, fullpath, (char *)NULL);
 	} else if (program_pid == -1) {
-		/* TODO */
+		goto fail;
 	}
 
 	waitpid(program_pid, NULL, 0);
@@ -302,6 +310,6 @@ int main(int argc, char *argv[])
 			free(dir_orig);
 		}
 	}
-
+fail:
 	free(fullpath);
 }
